d9bdbdf254fa7e60877ae6b03e17ee2b9beddd31
Jamie-Hickman <jamie.hickman35369@outlook.com>
2022-11-24 15:50:52 +0000

    Update Program.cs

--- activity1/Program.cs
+++ activity1/Program.cs
@@ -14,7 +14,7 @@ namespace activity1
 
             string[] new_sequence = sequence.Split(',');
 
-            int running_total = 0;
+            public int running_total = 0;
             int saved_total = 0;
             int final_total = 0;
             List<int> new_list = new List<int>() { };
